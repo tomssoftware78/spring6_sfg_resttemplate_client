359e7ff919755bb29e951f2b0bc402a74e576f00
Tom Van den Eynde <tom.van.den.eynde@ubench.com>
2024-01-04 11:29:40 +0100

    Add existing project files to git

---
+++ src/main/java/be/tvde/resttemplateclient/ResttemplateClientApplication.java
@@ -0,0 +1,13 @@
+package be.tvde.resttemplateclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResttemplateClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResttemplateClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/be/tvde/resttemplateclient/ResttemplateClientApplicationTests.java
@@ -0,0 +1,13 @@
+package be.tvde.resttemplateclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResttemplateClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
